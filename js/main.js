7e73e9ddf4155c180a9882452553621e96473460
Hasan Naseem <hn92@live.com>
2018-05-14 15:31:22 -0400

    lights are turning off and on

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+var button = document.querySelector('button');
+button.addEventListener('click', function() {
+  if (document.querySelector('button').className === 'switch off') {
+    switchOn();
+  } else {
+    switchOff();
+  }
+});
 
 
+function switchOff() {
+  document.querySelector('button').className = 'switch off'
+  document.querySelector('body').className = 'dark'
+  document.querySelector('.status').innerText = 'Hey, who turned off the lights?'
+}
+
+function switchOn() {
+  document.querySelector('button').className = 'switch on'
+  document.querySelector('body').className = 'light'
+  document.querySelector('.status').innerText = "It's so bright in here!"
+}
